8eccf4636fece272c50f80d54530a26700794221
JuanjoVillarejo <jota_villarejo22@hotmail.com>
2025-09-03 19:04:55 +0000

    añado boton cambiar

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,7 +3,13 @@ import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	const [color, setColor] = useState("red");
+	const [color, setColor] = useState("");
+
+	const cycleColor = () => {
+		if (color === "red") setColor("green");
+		else if (color === "green") setColor("yellow");
+		else setColor("red");
+	};
 
 	return (
 		<div className="traffic-light">
@@ -19,6 +25,7 @@ const Home = () => {
 				className={`light green ${color === "green" ? "active" : ""}`}
 				onClick={() => setColor("green")}
 			></div>
+			<button onClick={cycleColor} className="toggle-btn">Cambia</button>
 		</div>
 	);
 }
